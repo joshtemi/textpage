f7494572846a8ce24a1b3785b487c411ac313105
joshtemi <99566851+joshtemi@users.noreply.github.com>
2022-04-13 22:50:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,29 @@ if(mq.matches){
    const hasSubMenu = document.querySelectorAll(".has-submenu")
    const menuIcon = document.getElementById("menu-icon")
 
+
+   
+    // alternative method of writing a function
+    // function navSlide(){
+    // const menuIcon = document.getElementById("burger-icon")
+    const menu = document.querySelector(".burger")
+    const sidebar = document.querySelector(".nav-items")
+    const navItems = document.querySelector(".nav-items li")
+
+   
+    
+        
+        
    menuIcon.addEventListener("click",function(){
+       // Burger Animation
+       menuIcon.classList.toggle("closeAnimation")
+       // Burger Animation
        if(mainNav.style.display !="block"){
            mainNav.style.display = "block";
-           this.innerHTML = "X"
+        //    this.innerHTML = "X"
         }else{
             mainNav.style.display = "none"
-            this.innerHTML ="MENU"
+            // this.innerHTML ="MENU"
         }
 
 
@@ -22,4 +38,9 @@ if(mq.matches){
             
         }
    })
+       
+
+    
+
+
 }
